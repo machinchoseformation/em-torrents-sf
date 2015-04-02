ec0bad2cac3d5509b2dcbad1f0aec5472e8f66f8
guillaume <gsylvestre@gmail.com>
2015-04-02 16:51:51 +0200

    comments in command

--- src/AppBundle/Command/ParseKickAssCommand.php
+++ src/AppBundle/Command/ParseKickAssCommand.php
@@ -9,27 +9,34 @@
 
     class ParseKickAssCommand extends Command
     {
+        //base url to parse
+        //the ##page## will be replaced by the page number
         protected $baseUrl = "http://kickass.to/movies/##page##/?field=seeders&sorder=desc";
         protected $oi; //output interface
-        protected $doctrine;
-        protected $validator;
-        protected $kickass_parser;
-        protected $imdb_parser;
+        protected $doctrine; //doctrine service
+        protected $validator;   //validator service
+        protected $kickass_parser;  //parser for kickass torrents (service)
+        protected $imdb_parser;     //parser for imdb_parser (service)
+        protected $logger;  //log service
 
-        public function __construct($doctrine, $validator, $kickass_parser, $imdb_parser)
+        //see services.yml for injection
+        public function __construct($doctrine, $validator, $kickass_parser, $imdb_parser, $logger)
         {
-            parent::__construct();
+            parent::__construct(); //parent's contructor, required
             $this->doctrine = $doctrine;
             $this->validator = $validator;
             $this->kickass_parser = $kickass_parser;
             $this->imdb_parser = $imdb_parser;
+            $this->logger = $logger;
         }
 
+        //set the name, description, arguments and option
         public function configure()
         {
             $this
                 ->setName("em:parse:kickass")
                 ->setDescription("Extracts movies from KickAss Torrents")
+                //we can provide an optionnal number of "results" pages to parse from
                 ->addArgument(
                     'pages',
                     InputArgument::OPTIONAL,
@@ -37,11 +44,21 @@
                 );
         }
 
+        //called when em:parse:kickass in console
+        /**
+         * @param InputInterface $input
+         * @param OutputInterface $output
+         * @return int|null|void
+         */
         protected function execute(InputInterface $input, OutputInterface $output)
         {
+            //logs are in app/logs/
+            $this->logger->info("Parsing kickass !");
 
+            //referencable by other methods
             $this->oi = $output;
 
+            //get the optionnal argument, default to 3 pages
             $pages = $input->getArgument('pages');
             $pages = ($pages) ? $pages : 3;
 
@@ -49,9 +66,10 @@
             for($i=1;$i<=$pages;$i++){
                 $baseUrl = str_replace("##page##", $i, $this->baseUrl);
 
+                //some output
                 $output->writeln("<question>Parsing list page number $i...</question>");
 
-                //get all links to detailled page
+                //get all links to detailled page from the service
                 $detailLinks = $this->kickass_parser->parseListPage($baseUrl);
 
                 $output->writeln("Looking for new torrents...");
@@ -59,7 +77,9 @@
             }
         }
 
-
+        /**
+         * @param array array of detail page links
+         */
         protected function getAndSaveNewData(array $detailLinks)
         {
             $movieRepo = $this->doctrine->getRepository("AppBundle\Entity\Movie");
@@ -113,13 +133,19 @@
                 //save if valid
                 $this->saveEntity($torrent, "Torrent");
             }
+            return;
         }
 
+        //just to avoir typing that tag again
+        /**
+         * @param $message
+         */
         protected function showInfo($message)
         {
             $this->oi->writeln("<info>" . $message . "</info>");
         }
 
+        //save any kind of entity
         protected function saveEntity($entity, $type)
         {
             $em = $this->doctrine->getManager();
@@ -128,6 +154,10 @@
             $this->oi->writeln("<info>$type saved !</info>");
         }
 
+        /**
+         * @param $errors array of errors
+         * @param null $message global optionnal message
+         */
         protected function showValidationErrors($errors, $message = null)
         {
             if ($message){
